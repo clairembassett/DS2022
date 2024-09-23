78c553813e9199c399e904efbffefae0a4e1f473
clairembassett <157423196+clairembassett@users.noreply.github.com>
2024-09-23 03:45:29 +0000

    adding files to lab3

---
+++ my-work/lab3/mad.lib.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Write a silly adjective: " ADJECTIVE1
+read -p "2. Name a animal: " NOUN1
+read -p "3. Name a verb: " VERB1
+read -p "4. Write an adjective describing the weather: " ADJECTIVE2
+read -p "5. Write a verb that can be done on grass: " VERB2
+read -p "6. Name a place on grounds: " NOUN2
+read -p "7. Write a verb: " VERB3
+read -p "7. Write a buzz word: " ADJECTIVE4
+read -p "8. Name any food: " NOUN3
+
+echo "At UVA there are many $ADJECTIVE1 $NOUN1. I $VERB1 with $NOUN1 at the Rotunda on $ADJECTIVE2 afternoons. We like to $VERB2 on the lawn. When I'm at $NOUN2, I $VERB3 $ADJECTIVE4 $NOUN3."
